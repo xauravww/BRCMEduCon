9f18ed87acef6224a0918b428f59a449e8c236a0
xauravww <sauravmaheshwari8@gmail.com>
2023-09-10 14:48:59 +0530

    working on Student Dashboard and events

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.applandeo:material-calendar-view:1.9.0-rc03")
 }
