a3329773054e4ce808e8258f3db8bfe3dffe54ec
hkpanghal <112403425+hkpanghal@users.noreply.github.com>
2023-10-06 17:16:31 +0530

    added full screen feature in student gallery

--- app/src/main/java/com/education/brcmeducorn/adapter/GalleryRecyclerAdapter.kt
+++ app/src/main/java/com/education/brcmeducorn/adapter/GalleryRecyclerAdapter.kt
@@ -1,16 +1,40 @@
 package com.education.brcmeducorn.adapter
 
+import android.Manifest
+import android.app.Activity
+import android.app.Dialog
+import android.app.ProgressDialog.show
+import android.content.ContentValues
 import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.education.brcmeducorn.R
+import com.education.brcmeducorn.fragments.student_dashboard_fragments.GalleryFragment
 import com.squareup.picasso.Picasso
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.util.Random
 
-class GalleryRecyclerAdapter(val context: Context,var list: List<Int>):RecyclerView.Adapter<GalleryRecyclerAdapter.GalleryAdapterViewHolder>() {
+class GalleryRecyclerAdapter(
+    val context: Context,
+    var list: List<Int>
+):RecyclerView.Adapter<GalleryRecyclerAdapter.GalleryAdapterViewHolder>() {
 
     class GalleryAdapterViewHolder(val view: View):RecyclerView.ViewHolder(view)
     {
@@ -30,5 +54,30 @@ class GalleryRecyclerAdapter(val context: Context,var list: List<Int>):RecyclerV
     override fun onBindViewHolder(holder: GalleryAdapterViewHolder, position: Int) {
         val src = list[position]
         Picasso.get().load(src).into(holder.imgGallery)
+
+        holder.imgGallery.setOnClickListener {
+
+            showImageOnFullScreen(src)
+        }
     }
+
+    private fun showImageOnFullScreen(imageResource:Int)
+    {
+        val dialog = Dialog(context, android.R.style.Theme_Material_Light_NoActionBar_Fullscreen)
+        dialog.setContentView(R.layout.fragment_gallery_full_screen_image)
+
+        val fullScreenImageView = dialog.findViewById<ImageView>(R.id.fullScreenImageView)
+        val downloadButton = dialog.findViewById<ImageButton>(R.id.downloadButton)
+
+        Picasso.get().load(imageResource).into(fullScreenImageView)
+
+        downloadButton.setOnClickListener {
+            Toast.makeText(context,"clicked",Toast.LENGTH_SHORT).show()
+        }
+
+        dialog.show()
+    }
+
+
+
 }
